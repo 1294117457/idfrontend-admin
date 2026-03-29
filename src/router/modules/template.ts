949a42d0748048fb71e626abb5df406c4c21be03
unknown <zhouch2@g-bits.com>
2026-03-29 23:20:03 +0800

    zong

--- src/router/modules/template.ts
+++ src/router/modules/template.ts
@@ -1,14 +1,16 @@
 import ScoreTemplate from '@/views/template/scoreTemplate.vue'
 import ScoreAttribute from '@/views/template/scoreAttribute.vue'
 import DemandTemplate from '@/views/template/demandTemplate.vue'
-import SettingIcon from '@/assets/icons/setting.vue' // 导入 setting.vue 组件
+import FieldConfig from '@/views/template/fieldConfig.vue'
+import SettingIcon from '@/assets/icons/setting.vue'
 
 export default {
   path: 'demand',
   meta: { title: '模板管理', icon: SettingIcon, sort: 3, requiresRoles: ['admin', 'super_admin'] },
   children: [
-    { path: 'scoreTemplate', component: ScoreTemplate, meta: { title: '分数模板' } },
-    { path: 'scoreAttribute', component: ScoreAttribute, meta: { title: '属性管理' } },
+    { path: 'scoreTemplate',  component: ScoreTemplate,  meta: { title: '分数模板' } },
     { path: 'demandTemplate', component: DemandTemplate, meta: { title: '需求模板' } },
+    { path: 'scoreAttribute', component: ScoreAttribute, meta: { title: '属性管理' } },
+    { path: 'fieldConfig',    component: FieldConfig,    meta: { title: '字段管理' } },
   ],
 }
