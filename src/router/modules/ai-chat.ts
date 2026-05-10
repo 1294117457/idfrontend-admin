f47d3c914e1ab23931ff8de0015454aca67ace7b
zch <1294117457@qq.com>
2026-05-11 00:12:24 +0800

    mcp

--- src/router/modules/ai-chat.ts
+++ src/router/modules/ai-chat.ts
@@ -4,5 +4,5 @@ import SettingIcon from '@/assets/icons/setting.vue'
 export default {
   path: 'ai-chat',
   component: AiChat,
-  meta: { title: 'AI助手', icon: SettingIcon, sort: 97 }
+  meta: { title: 'AI助手', icon: SettingIcon, sort: 97,hidden:true }
 }
